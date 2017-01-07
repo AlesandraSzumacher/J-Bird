46c3e1e8000cea571f7da729ddaa283eafe75ce5
marianka <mania600@gmail.com>
2017-01-07 21:44:19 +0100

    added enum with start width and height for player

---
+++ jBird/logic/GameRun.py
@@ -0,0 +1,11 @@
+import pygame
+from jBird.logic.Game import Game
+from jBird.logic.Player import Player
+
+
+# TODO put intro here
+game = Game()
+player = Player("Mariusz Jurkiewicz")
+
+while 1:
+    pass
--- jBird/utils/ScreenSize.py
+++ jBird/utils/ScreenSize.py
@@ -3,3 +3,7 @@ from enum import Enum
 class ScreenSize(Enum):
     WIDTH = 800
     HEIGHT = 600
+
+class PlayerPos(Enum):
+    START_WIDTH = 100
+    START_HEIGHT = 100
