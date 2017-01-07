fa5e6fa9ac134835d3d1217a33a5aacf000f31a3
Aleksandra <ola.szumacher@protonmail.com>
2017-01-07 20:55:49 +0100

    Merge

--- jBird/utils/ScreenSize.py
+++ jBird/utils/ScreenSize.py
@@ -1,14 +1,10 @@
 from enum import Enum
 
 class ScreenSize(Enum):
-<<<<<<< HEAD
     WIDTH = 1200
     HEIGHT = 800
-=======
-    WIDTH = 800
-    HEIGHT = 600
+
 
 class PlayerPos(Enum):
     START_WIDTH = 100
     START_HEIGHT = 100
->>>>>>> master
