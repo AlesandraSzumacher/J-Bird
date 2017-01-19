fabc64372c75b12870f5c42437d1f847ab84ad93
Aleksandra <ola.szumacher@protonmail.com>
2017-01-19 17:06:36 +0100

    added dosc

--- jBird/utils/ScreenSize.py
+++ jBird/utils/ScreenSize.py
@@ -14,6 +14,7 @@ class PlayerPos(Enum):
 
 
 class BoardSize(Enum):
+    """Enum containing size of the Board."""
     TILE_WIDTH = 80
     TILE_HEIGHT = 50
     CUBE_BREAK_HEIGHT = 50
@@ -21,16 +22,19 @@ class BoardSize(Enum):
 
 
 class TileColor(Enum):
+    """Enum containing color of the Tile in RGB."""
     START_COLOR = (255, 255, 168)
     TOUCH_COLOR = (255, 0, 0)
 
 
 class WallColor(Enum):
+    """Enum containing color of the Wall in RGB."""
     LEFT_WALL_COLOR = (0, 0, 30)
     RIGHT_WALL_COLOR = (166, 41, 79)
 
 
 class Positions(Enum):
+    """Enum containing starting position of chicken."""
     CHICKEN_INIT_POSITION = [ScreenSize.WIDTH.value / 2 - 30, 10]
     BOARD_DOWN = 150
 
