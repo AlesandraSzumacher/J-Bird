b8eeb1643bcd52b9420d1f19f1dcc8f4567023fd
marianka <mania600@gmail.com>
2017-02-04 17:18:01 +0100

    deleted unused line, formatted text.

--- jBird/utils/LevelsUtils.py
+++ jBird/utils/LevelsUtils.py
@@ -5,7 +5,7 @@ class NumberOfTiles(Enum):
     """Enum containing number of tiles for all levels."""
     LEVEL_1 = 28
 
+
 class NumberOfNeededTouches(Enum):
     """Enum containing number of needed touches for specific level."""
     Level_1 = 1
-3
